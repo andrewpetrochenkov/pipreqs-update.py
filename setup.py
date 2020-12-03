7db07bf4148c4090ebd919392c0eab8022d5f896
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:12:14 +0300

    2020:12:04 00:12

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pipreqs-update',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/pipreqs-update']
